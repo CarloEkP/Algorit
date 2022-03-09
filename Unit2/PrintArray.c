1f627294031140cb51976d3db78ad31af4b708d1
CarloEkP <2109058@upy.edu.mx>
2022-03-08 21:54:44 -0600

    Add PrintArray

---
+++ Unit2/PrintArray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int myArray[3]={5, 2, 3};
+    int i = 0;
+
+    printf("[");
+
+    void printArray(int myArray[3], int arrayLength){
+        while (i < 3){
+            printf("%d", myArray[i]);
+            if(i < 2){
+                printf(",");
+            }
+            i++;
+        }
+    }
+
+    printArray(myArray, 3);
+    printf("]\n");
+
+    return 0;
+}
+
